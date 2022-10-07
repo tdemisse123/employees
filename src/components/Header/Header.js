5a24c05a46cc6db07b81d725d6de62bf32fdea48
Tigst Demisse <tdemisse123@gmail.com>
2022-10-07 11:50:55 -0500

    fontawesome icon is added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useNavigate } from "react-router"
 import { navigate } from "react-router-dom"
-//import {faAngleLeft} from "@fortawesome/free-regular-svg-icons"
+import {faArrowAltCircleLeft} from "@fortawesome/free-regular-svg-icons"
 
 
 export default function Header(props) {
@@ -12,10 +12,8 @@ export default function Header(props) {
              {window.location.pathname !== '/' &&
                 <div onClick={()=>{
                 navigate('/')
-            }}
-            style={{paddingRight: "20px", paddingLeft: "10px", fontSize: "20px", fontWeight: "800", cursor:"pointer"}}>
-                {"<"}
-            {/* <FontAwesomeIcon icon={faAngleLeft} /> */}
+            }}>
+          <FontAwesomeIcon icon={faArrowAltCircleLeft} /> 
             </div> 
             }  
 
